3e6870f036a1e59dbd4128e9adec8bb57d5a8083
Gaurab Kumar <progaurab@gmail.com>
2019-05-16 14:08:12 +0530

    Bar Code Scanner

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,32 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
 
-    document.getElementById("version").innerHTML = device.cordova;
-    document.getElementById("model").innerHTML = device.model;
-    document.getElementById("platform").innerHTML = device.platform;
-    document.getElementById("uuid").innerHTML = device.uuid;
-    document.getElementById("androidVersion").innerHTML = device.version;
-    document.getElementById("manufacturer").innerHTML = device.manufacturer;
-    document.getElementById("isvirtual").innerHTML = device.isVirtual;
-    document.getElementById("serialNo").innerHTML = device.serial;
+document.getElementById("scan").addEventListener("click", scan);	
 
+function scan() {
+    console.log("scan button clicked");
+
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        },
+  
+        {
+            preferFrontCamera : true, // iOS and Android
+            showFlipCameraButton : true, // iOS and Android
+            showTorchButton : true, // iOS and Android
+            torchOn: true, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt : "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations : true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        }                
+    );
 }
